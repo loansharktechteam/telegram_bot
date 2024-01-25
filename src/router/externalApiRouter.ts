a8d222847e3d92595d1dd471de392f6315cd0a68
gap <tyehcwc402004@gmail.com>
2024-01-26 00:32:11 +0800

    resume

--- src/router/externalApiRouter.ts
+++ src/router/externalApiRouter.ts
@@ -12,36 +12,36 @@ router.post('/getScoreByAddress', async function (req: Request, res: Response) {
 });
 
 router.get('/checkHasDeposit', async function (req: Request, res: Response) {
-    res.status(200).json({})
-    return 
-    // const address = req.query.address;
-    // // const { address } = req.params
-    // console.log(`address`, address)
-    // let result = {
-    //     "code":0,
-    //     "data":false, //   If the task verification passes, return true; if not, return false
-    //     "detailMsg":"",
-    //     "error_code":"0",
-    //     "error_message":"",
-    //     "msg":""
-    // }
-    // if(typeof(address)!=='string'){
-    //     result.error_code='-1'
-    //     result.error_message='wrong input address type'
-    //     res.status(200).json(result)
-    //     return 
-    // }
+    // res.status(200).json({})
+    // return 
+    const address = req.query.address;
+    // const { address } = req.params
+    console.log(`address`, address)
+    let result = {
+        "code":0,
+        "data":false, //   If the task verification passes, return true; if not, return false
+        "detailMsg":"",
+        "error_code":"0",
+        "error_message":"",
+        "msg":""
+    }
+    if(typeof(address)!=='string'){
+        result.error_code='-1'
+        result.error_message='wrong input address type'
+        res.status(200).json(result)
+        return 
+    }
  
-    // let allAllLowerLetter = address.toLowerCase()
-    // let getScoreByAddressRespond:any = await priceLogginService.getScoreByAddress(allAllLowerLetter)
-    // console.log(`getScoreByAddressRespond`, getScoreByAddressRespond)
-    // if (getScoreByAddressRespond?.result?.[0]?.marks ?? 0 > 0) {
-    //     result.data = true
-    // } else {
-    //     result.data = false
-    // }
-    // res.status(200).json(result)
-    // return
+    let allAllLowerLetter = address.toLowerCase()
+    let getScoreByAddressRespond:any = await priceLogginService.getScoreByAddress(allAllLowerLetter)
+    console.log(`getScoreByAddressRespond`, getScoreByAddressRespond)
+    if (getScoreByAddressRespond?.result?.[0]?.marks ?? 0 > 0) {
+        result.data = true
+    } else {
+        result.data = false
+    }
+    res.status(200).json(result)
+    return
 });
 
 export default router;
