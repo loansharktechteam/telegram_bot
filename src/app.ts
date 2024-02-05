8ec6826380c8d4407c9928e6c2baf9582085f4f1
gap <tyehcwc402004@gmail.com>
2024-02-05 18:06:33 +0800

    open  cronjob

--- src/app.ts
+++ src/app.ts
@@ -85,8 +85,8 @@ const job = new CronJob.CronJob("* * * * *", function () {
     // cronJobService.startLiquidation();
 })
 
-const jobScoreSystem = new CronJob.CronJob("0 * * * *", function () {
+const jobScoreSystem = new CronJob.CronJob("0 23 * * *", function () {
     console.log(`triggerScoreSystem cron job by bot ${new Date()}`)
     cronJobService.startScoreSystem();
 })
-// jobScoreSystem.start()
+jobScoreSystem.start()
